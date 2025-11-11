44efcc2a8334707906cab13a436b6b63eb0b5b03
Chris Brown <77508021+peakschris@users.noreply.github.com>
2025-11-11 14:16:09 -0500

    windows fixes

---
+++ .github/workflows/build_release.bat
@@ -0,0 +1,4 @@
+call bazel build //:for_windows_platforms --noshow_progress
+
+move bazel-out\windows_x86_64-opt\bin\toml2json.exe toml2json_windows.exe
+certutil -hashfile toml2json_windows.exe SHA256 > SHA256.txt
