c1c8857be00e1b235c5ed133deea9310945ff325
David Zbarsky <dzbarsky@gmail.com>
2026-02-25 22:25:48 -0500

    fixes

--- .github/workflows/build_release_windows_amd64.bat
+++ .github/workflows/build_release_windows_amd64.bat
@@ -1,4 +1,4 @@
-call bazel build //:for_x86_64-pc-windows-msvc
+call bazel --output_user_root=C:/b build //:for_x86_64-pc-windows-msvc
 
 move bazel-out\x86_64-pc-windows-msvc-opt\bin\toml2json.exe toml2json_windows_amd64.exe
 certutil -hashfile toml2json_windows_amd64.exe SHA256 > SHA256_windows_amd64.txt
--- .github/workflows/build_release_windows_arm64.bat
+++ .github/workflows/build_release_windows_arm64.bat
@@ -1,4 +1,4 @@
-call bazel build //:for_aarch64-pc-windows-msvc
+call bazel --output_user_root=C:/b build //:for_aarch64-pc-windows-msvc
 
 move bazel-out\aarch64-pc-windows-msvc-opt\bin\toml2json.exe toml2json_windows_arm64.exe
 certutil -hashfile toml2json_windows_arm64.exe SHA256 > SHA256_windows_arm64.txt
