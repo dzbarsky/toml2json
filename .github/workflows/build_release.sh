f242eb02ab53b69bb12e572fffe64c052f8ee905
David Zbarsky <dzbarsky@gmail.com>
2026-04-28 09:53:07 -0400

    Fix windows exe

--- .github/workflows/build_release.sh
+++ .github/workflows/build_release.sh
@@ -6,14 +6,14 @@ bazel build //:for_all_platforms
 
 copy_out() {
     src="$(bazel cquery --output=files "$1")"
-    cp "$src" "$2"
+    cp "$src" "$2${3:-}"
 }
 
 copy_out //:for_aarch64-unknown-linux-musl toml2json_linux_arm64
 copy_out //:for_x86_64-unknown-linux-musl toml2json_linux_amd64
 copy_out //:for_aarch64-apple-darwin toml2json_darwin_arm64
 copy_out //:for_x86_64-apple-darwin toml2json_darwin_amd64
-copy_out //:for_aarch64-pc-windows-msvc toml2json_windows_arm64
-copy_out //:for_x86_64-pc-windows-msvc toml2json_windows_amd64
+copy_out //:for_aarch64-pc-windows-msvc toml2json_windows_arm64 .exe
+copy_out //:for_x86_64-pc-windows-msvc toml2json_windows_amd64 .exe
 
 shasum -a 256 toml2json_* > SHA256.txt
